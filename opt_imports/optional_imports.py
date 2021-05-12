a1d877a43fbf809cf557d66e28e021c6c53201e3
Kevin Sawade <37301807+kevinsawade@users.noreply.github.com>
2021-05-12 12:38:51 +0200

    Added licensing to optional_imports.py

--- opt_imports/optional_imports.py
+++ opt_imports/optional_imports.py
@@ -1,3 +1,32 @@
+# -*- coding: utf-8 -*-
+# optional_imports.py
+
+# Copyright (c) 2021, Kevin Sawade (kevin.sawade@uni-konstanz.de)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+# * Neither the name of the copyright holders nor the names of any
+#   contributors may be used to endorse or promote products derived
+#   from this software without specific prior written permission.
+#
+# This file is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 2.1
+# of the License, or (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# Find the GNU Lesser General Public License under <http://www.gnu.org/licenses/>.
 """Optional imports of python packages.
 
 ToDo:
