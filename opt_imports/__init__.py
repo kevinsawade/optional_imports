99ad2070db21571cd28c2f6dd7717945363fa4a8
Kevin Sawade <kevin.sawade@uni-konstanz.de>
2021-06-07 09:27:37 +0200

    Added some debug os stuff into __init__

--- opt_imports/__init__.py
+++ opt_imports/__init__.py
@@ -3,5 +3,5 @@ from .sub_package_1 import __init__
 from .sub_package_2 import __init__
 import os
 print(os.getcwd())
-print(os.listdir(os.getcwd()))
+print(os.listdir(os.getcwd() + '/opt_imports/'))
 from .sub_project import __init__
